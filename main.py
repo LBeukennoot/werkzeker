42ded8d1794b5a1562f2b1bd2ffd19a1f765f80b
LBeukennoot <lars@beukenkamp.net>
2026-06-10 14:16:30 +0200

    Adds text_input functionality

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 customer_data = []
 
 def select_option_input(field_name: str, options: list) -> str:
-    print(f'Select {field_name}:')
+    print(f'Select {field_name} by entering the corresponding number:')
     for i in range(1, len(options) + 1):
         print(f'[{i}] {options[i-1]}')
     
@@ -19,18 +19,36 @@ def select_option_input(field_name: str, options: list) -> str:
             return options[user_selection_number - 1]
         else:
             print(f"Incorrect input, enter a number between 1 and {len(options)}.")
+            
+def text_input(field_name:str, field_type: type):
+    print(f'Enter your {field_name}')
+    
+    while True:
+        user_selection = input('> ')
+        
+        if field_type == int:
+            if not user_selection.isdigit():
+                print(f"Incorrect input, enter a number.")
+                continue
+            else:
+                return int(user_selection)
+        
+        if field_type == str:
+            return user_selection
+        else:
+            print('Incorrect input, try again')
 
-def save_data_from_input():
+def get_data_from_input():
     input_fields = {
-        # 'name': {
-        #     'type': 'string'
-        # },
-        # 'age': {
-        #     'type': 'int'
-        # },
-        # 'expenses': {
-        #     'type': 'int'
-        # },
+        'name': {
+            'type': str
+        },
+        'age': {
+            'type': int
+        },
+        'expenses': {
+            'type': int
+        },
         'customer_type': {
             'type': 'string',
             'options': ['new', 'existing', 'premium']
@@ -39,10 +57,11 @@ def save_data_from_input():
     
     for field_name in input_fields:
         field_data = input_fields[field_name]
-        if 'options' in field_data:
-            select_option_input(field_name, field_data['options'])
-        else:
-            input(f"Enter your {field_name.replace('_', ' ')} \n> ")
+        field_name_sanitized = field_name.replace('_', ' ')
         
-save_data_from_input()
-# print(customer_data)
+        if 'options' in field_data:
+            select_option_input(field_name_sanitized, field_data['options'])
+        else:
+            text_input(field_name_sanitized, field_data['type'])
+        
+get_data_from_input()
