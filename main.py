143bd0c340289c17f49ce0e6c828d2fdf36cc054
LBeukennoot <lars@beukenkamp.net>
2026-06-10 15:36:24 +0200

    Adds functions explanation

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 customers = []
 
 def select_option_input(input_question: str, options: list) -> str:
+    """
+    Function that lets the user choose one of the defined options.
+    Using the while loop, it constantly loops over the same code
+    untill the user enters a valid option, what exits the loop with
+    'return'. The function is reused in the creation of customers,
+    but also to check if another customer is going to be added.
+    The user can only enter a number (text is not allowed) and a 
+    number that actually is an option. Any given inputis handled 
+    and a suited 'error' is given to the user.
+    """
+    
     print(input_question)
     for i in range(1, len(options) + 1):
         print(f'[{i}] {options[i-1]}')
@@ -21,6 +32,12 @@ def select_option_input(input_question: str, options: list) -> str:
             print(f"Incorrect input, enter a number between 1 and {len(options)}.")
 
 def text_input(field_name:str, field_type: type):
+    """
+    Allows the user to enter either a string or integer as input.
+    The function props define which variable type is allowed, and
+    gives a suitable 'error' when an incorrect value has been entered.
+    """
+    
     print(f'Enter your {field_name}')
     
     while True:
@@ -39,6 +56,15 @@ def text_input(field_name:str, field_type: type):
             print('Incorrect input, try again')
 
 def save_data_from_input():
+    """
+    Combines the two input functions and asks the user for input
+    using the input() functionality. The field options are defined
+    at input_fields, including field types (and options). The
+    function also handles whether the user wants to insert another
+    customer, and is saved globally so other functions can use
+    the data.
+    """
+    
     input_fields = {
         'name': {
             'type': str
@@ -55,9 +81,7 @@ def save_data_from_input():
         }
     }
     
-    done_adding_customers = False
-    
-    while not done_adding_customers:
+    while True:
         customer_data = {}
     
         for field_name in input_fields:
@@ -81,29 +105,36 @@ def save_data_from_input():
             return
     
 def generate_advice(customer):
-    
-    advice = "Standaard dienstverlening"
-    
-    if customer['age'] >= 67:
-        advice = 'AOW-check en extra begeleiding'
-    
-    if customer['monthly_expenses'] > 100:
-        advice = 'Check aanvullende regelingen / samenloop'
+    """
+    Generates advice based on the user input. Prioritized data
+    gets returned first and ignores the other options.
+    """
     
     if customer['customer_type'] == 'premium':
-        advice = 'Intensievere begeleiding (complex dossier)'
+        return 'Intensievere begeleiding (complex dossier)'
+        
+    if customer['monthly_expenses'] > 100:
+        return 'Check aanvullende regelingen / samenloop'
     
-    return advice
+    if customer['age'] >= 67:
+        return 'AOW-check en extra begeleiding'
+    
+    return "Standaard dienstverlening"
 
 
 def print_summary():
+    """
+    Combines all functionalities and prints a summary of all
+    submitted customers.
+    """
+    
     save_data_from_input()
     
     i = 0
     
     while i < len(customers):
         advice = generate_advice(customers[i])
-        print(f'\n{customers[i]['name']}:\n{advice}\n')
+        print(f'{customers[i]['name']}:\n{advice}\n')
         i +=1
 
 print_summary()
